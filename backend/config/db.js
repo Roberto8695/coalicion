48e1f7dbc15dfb51351ee3b4bdb9ed0313d123bd
Otrebor19 <96426304+Otrebor19@users.noreply.github.com>
2025-10-31 13:29:07 -0400

    Mejoras en la conexion

--- backend/config/db.js
+++ backend/config/db.js
@@ -5,11 +5,20 @@ dotenv.config(); // Asegúrate de cargar las variables de entorno
 
 // Configuración del Pool usando variables de entorno
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    // ===========================================
+    // ✨ AÑADE ESTO PARA SOLUCIONAR EL ERROR SSL/TLS required EN RENDER
+    // ===========================================
     ssl: {
         rejectUnauthorized: false
-    }
+    } 
+    // ===========================================
 });
+
 // Opcional: Para verificar la conexión al iniciar la aplicación
 pool.on('connect', () => {
     console.log('Cliente de PostgreSQL conectado');
